ef0014b9d9d58031389527f9059cf1c0760573df
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-29 19:45:29 -0500

    Taller 24

--- src/modules/pokemon/page/PokemonPage.vue
+++ src/modules/pokemon/page/PokemonPage.vue
@@ -2,17 +2,36 @@
   <h1>Juego Pokemon</h1>
   <PokemonImg :pokemonId="151" :muestraPokemon="false" />
 
-  <PokemonOps />
+  <PokemonOps :opciones="arreglo" />
 </template>
 
 <script>
 import PokemonImg from "../components/PokemonImg.vue";
 import PokemonOps from "../components/PokemonOps1.vue";
+import obtenerFachadaPokemon from "../helpers/clientePokemonAPI";
 
 export default {
+  data(){
+    return{
+       arreglo: []
+    };
+      
+  },
+
   components: {
     PokemonImg,
     PokemonOps,
+  },
+  methods: {
+    async cargaJuegoInicial() {
+      const arregloPokemons = await obtenerFachadaPokemon();
+      console.log(arregloPokemons);
+      this.arreglo = arregloPokemons;
+    },
+  },
+  mounted() {
+    console.log("Se monto el componente");
+    this.cargaJuegoInicial();
   },
 };
 </script>
